bce0fdee6761e2b0bb97ba29c12717efd5919718
silvia pintus <silviapintus.93@gmail.com>
2021-05-16 13:53:47 +0100

    game logic done

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2){
+  document.querySelector(".container h1").innerHTML= "Draw";
+}
+
+else if(randomNumber1 > randomNumber2){
+  document.querySelector(".container h1").innerHTML= "Player 1 wins!";
+}
+
+else{
+  document.querySelector(".container h1").innerHTML= "Player 2 wins!";
+}
